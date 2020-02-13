2bf6632cacaf0880e54e7605a4529bc819d7f8b0
Luca Santarella <luca.santarella@gmail.com>
2020-02-13 16:08:29 -0500

    Lab Cash Register

---
+++ src/edu/njit/cs114/LabCashRegister.java
@@ -0,0 +1,58 @@
+package edu.njit.cs114;
+
+
+/**
+ * Author: Ravi Varadarajan
+ * Date created: 2/11/20
+ */
+public class LabCashRegister {
+
+    private static final int INFINITY = Integer.MAX_VALUE;
+
+    private int [] denominations;
+
+    /**
+     * Constructor
+     * @param denominations values of coin types not in any order
+     * @throws Exception when a coin of denomination 1 does not exist
+     */
+    public LabCashRegister(int [] denominations) throws Exception {
+        /**
+         * Complete code here
+         */
+    }
+
+    /**
+     * Find the minimum number of coins to make change
+     * @param value value for which to make change
+     * @return
+     */
+    public int minimumCoinsForChange(int value) {
+        /**
+         * Complete code here
+         */
+    }
+
+    public static void main(String [] args) throws Exception {
+        LabCashRegister reg = new LabCashRegister(new int [] {50, 25, 10, 5, 1});
+        // should have a total of 6 coins
+        System.out.println("Minimum coins to make change for " + 48
+                + " from {50,25,10,5,1} = "+ reg.minimumCoinsForChange(48));
+        // should have a total of 3 coins
+        System.out.println("Minimum coins to make change for " + 56
+                + " from {50,25,10,5,1} = "+ reg.minimumCoinsForChange(56));
+        reg = new LabCashRegister(new int [] {25, 10, 1});
+        // should have a total of 6 coins
+        System.out.println("Minimum coins to make change for " + 33
+                + " from {25,10,1} = "+ reg.minimumCoinsForChange(33));
+        reg = new LabCashRegister(new int [] {1, 7, 24, 42});
+        // should have a total of 2 coins
+        System.out.println("Minimum coins to make change for " + 48
+                + " from {1,7,24,42} = "+ reg.minimumCoinsForChange(48));
+        reg = new LabCashRegister(new int [] {50, 1, 3, 16, 30});
+        // should have a total of 3 coins
+        System.out.println("Minimum coins to make change for " + 35
+                + " from {50,1,3,16,30} = "+ reg.minimumCoinsForChange(35));
+    }
+}
+
