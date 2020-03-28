44660bb815b8c7e567eae9aa81232a9f866fefc7
Luca Santarella <luca.santarella@gmail.com>
2020-03-28 13:35:21 -0400

    Added homework

---
+++ src/edu/njit/cs114/AnagramFinderListImpl.java
@@ -0,0 +1,94 @@
+package edu.njit.cs114;
+
+import java.io.IOException;
+import java.util.*;
+
+/**
+ * Author: Ravi Varadarajan
+ * Date created: 3/22/20
+ */
+public class AnagramFinderListImpl extends AbstractAnagramFinder {
+
+    private List<WordArrPair> wordArrList = new ArrayList<>();
+
+    private class WordArrPair implements Comparable<WordArrPair> {
+        public final String word;
+        public final char[] charArr;
+
+        public WordArrPair(String word) {
+            this.word = word;
+            charArr = word.toCharArray();
+            Arrays.sort(charArr);
+        }
+
+        public boolean isAnagram(WordArrPair wordArrPair) {
+            /**
+             * To be completed
+             * Compare charArr already sorted
+             */
+            return Arrays.equals(this.charArr, wordArrPair.charArr);
+        }
+
+        @Override
+        public int compareTo(WordArrPair wordArrPair) {
+            return this.word.compareTo(wordArrPair.word);
+        }
+
+
+    }
+
+
+    @Override
+    public void clear() {
+        wordArrList.clear();
+    }
+
+    @Override
+    public void addWord(String word) {
+        /**
+         * Create a word pair object and add it to list
+         */
+        wordArrList.add(new WordArrPair(word));
+    }
+
+    @Override
+    public List<List<String>> getMostAnagrams() {
+        Collections.sort(wordArrList);
+        ArrayList<List<String>> mostAnagramsList = new ArrayList<>();
+        /**
+         * To be completed
+         * Repeatedly do this:
+         * (a)Each wordPair in list is compared to others to identify all its anagrams;
+         * (b) add the anagram words to the same group;
+         *      if there are no anagrams, single word forms a group
+         * (c) remove these words from wordArrList
+         */
+        return mostAnagramsList;
+    }
+
+    public static void main(String[] args) {
+        AnagramFinderListImpl finder = new AnagramFinderListImpl();
+        finder.clear();
+        long startTime = System.nanoTime();
+        int nWords = 0;
+        try {
+            nWords = finder.processDictionary("words.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        List<List<String>> anagramGroups = finder.getMostAnagrams();
+        long estimatedTime = System.nanoTime() - startTime;
+        double seconds = ((double) estimatedTime / 1000000000);
+        System.out.println("NUmber of words : " + nWords);
+        System.out.println("Number of groups of words with maximum anagrams : "
+                + anagramGroups.size());
+        if (!anagramGroups.isEmpty()) {
+            System.out.println("Length of list of max anagrams : " + anagramGroups.get(0).size());
+            for (List<String> anagramGroup : anagramGroups) {
+                System.out.println("Anagram Group : " + anagramGroup);
+            }
+        }
+        System.out.println("Time : " + seconds);
+
+    }
+}
